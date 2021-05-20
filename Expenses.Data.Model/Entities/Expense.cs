2489368aafe0acc21e19166d3e3acf554281e66f
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2021-05-20 16:13:41 +0200

    🚧Clean everythings

---
+++ Expenses.Data.Model/Entities/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expenses.Data.Model.Entities
+{
+    public class Expense
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string Comment { get; set; }
+        public string UserId { get; set; }
+        public bool? IsDeleted { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
